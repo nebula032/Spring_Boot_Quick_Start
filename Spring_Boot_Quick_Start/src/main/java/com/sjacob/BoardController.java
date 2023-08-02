b719e0863192ffd12af6d230dac07b3eff21e958
sjacob <nebula032@naver.com>
2023-08-02 14:36:15 +0900

    Chapter01

--- Spring_Boot_Quick_Start/src/main/java/com/sjacob/BoardController.java
+++ Spring_Boot_Quick_Start/src/main/java/com/sjacob/BoardController.java
@@ -1,7 +1,13 @@
 package com.sjacob;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sjacob.domain.BoardVO;
 
 @RestController
 public class BoardController {
@@ -15,5 +21,32 @@ public class BoardController {
 		return "Hello : " + name;
 	}
 	
-
+	@GetMapping("getBoard")
+	public BoardVO getBorad() {
+		BoardVO board = new BoardVO();
+		board.setSeq(1);
+		board.setTitle("스프링부트");
+		board.setWriter("boot");
+		board.setContent("테스트입니다");
+		board.setRegDate(new Date());
+		board.setCnt(0);
+		return board;
+	}
+	
+	@GetMapping("getBoardList")
+	public List<BoardVO> getBoradList() {
+		List<BoardVO> boardList = new ArrayList<BoardVO>();
+		for (int i = 1; i <= 10; i++) {
+			BoardVO board = new BoardVO();
+			board.setSeq(i);
+			board.setTitle("스프링부트" + i);
+			board.setWriter("boot" + i);
+			board.setContent("테스트입니다");
+			board.setRegDate(new Date());
+			board.setCnt(0);
+			boardList.add(board);
+		}
+		return boardList;
+	}
+	
 }
