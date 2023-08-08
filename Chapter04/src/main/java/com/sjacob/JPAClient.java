d0678e2163145d182da1b4d8138cac2bd2249b5a
sjacob <nebula032@naver.com>
2023-08-09 00:36:28 +0900

    JPA persistence context

--- Chapter04/src/main/java/com/sjacob/JPAClient.java
+++ Chapter04/src/main/java/com/sjacob/JPAClient.java
@@ -1,6 +1,8 @@
 package com.sjacob;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -22,20 +24,38 @@ public class JPAClient {
 			// Transaction 시작
 			tx.begin();
 			
-			Board board = new Board();
-			board.setTitle("JPA 제목");
-			board.setWrtier("작성자");
-			board.setContent("내용");
-			board.setCreateDate(new Date());
-			board.setCnt(0L);
-			
 			// 글 등록
-			em.persist(board);
-			tx.commit();
+//			Board board = new Board();
+//			board.setTitle("JPA 제목");
+//			board.setWrtier("작성자");
+//			board.setContent("내용");
+//			board.setCreateDate(new Date());
+//			board.setCnt(0L);
+//			
+//			em.persist(board);
+//			tx.commit();
 			
 			// 글 상세 조회
 //			Board seacrchBoard = em.find(Board.class, 1L);
 //			System.out.println("---> " + seacrchBoard);
+			
+			// 글 수정
+//			Board board = em.find(Board.class, 1L);
+//			board.setTitle("수정");
+//			tx.commit();
+			
+			// 글 삭제
+//			Board board1 = em.find(Board.class, 1L);
+//			em.remove(board1);
+//			tx.commit();
+			
+			// 글 목록 조회
+			String jpql = "select b from Board b order by b.seq desc";
+			List<Board> boardList = em.createQuery(jpql, Board.class).getResultList();
+			for (Board board : boardList) {
+				System.out.println("---> " + board);
+			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 			tx.rollback();
--- Chapter04/src/main/java/com/sjacob/domain/Board.java
+++ Chapter04/src/main/java/com/sjacob/domain/Board.java
@@ -9,15 +9,10 @@ import javax.persistence.*;
  *
  */
 @Entity
-@SequenceGenerator(name = "BOARD_SEQ_GENERATOR",
-				sequenceName = "BOARD_SEQUENCE",
-				initialValue = 1,
-				allocationSize = 1)
 public class Board {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE,
-					generator = "BOARD_SEQ_GENERATOR")
+	@GeneratedValue
 	private Long seq;
 	private String title;
 	private String wrtier;
