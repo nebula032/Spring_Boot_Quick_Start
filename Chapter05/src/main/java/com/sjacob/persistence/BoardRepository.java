880ed6db87b63226e259cbddc49a6d766122188d
sjacob <nebula032@naver.com>
2023-08-10 01:05:58 +0900

    Chapter05 Spring Data JPA Quick Start(2)

--- Chapter05/src/main/java/com/sjacob/persistence/BoardRepository.java
+++ Chapter05/src/main/java/com/sjacob/persistence/BoardRepository.java
@@ -1,9 +1,24 @@
 package com.sjacob.persistence;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import com.sjacob.domain.Board;
 
 public interface BoardRepository extends CrudRepository<Board, Long> {
-
+	List<Board> findByTitle(String searchKeyword);
+	
+	List<Board> findByContentContaining(String searchKeyword);
+	
+	List<Board> findByTitleContainingOrContentContaining(String title, String content);
+	
+	List<Board> findByTitleContainingOrderBySeqDesc(String searchKeyword);
+	
+//	List<Board> findByTitleContaining(String searchKeyword, Pageable paging);
+	
+	Page<Board> findByTitleContaining(String searchKeyword, Pageable paging);
 }
+
